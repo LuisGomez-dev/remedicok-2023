71502068f205384034379addfe09c5c4a0822d48
Luis Gomez <luis.e.gomez.sainz@gmai.com>
2022-12-05 18:12:33 -0600

    integracion demo localstorage

--- front_a/vue.config.js
+++ front_a/vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/rm/f1',
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 })
